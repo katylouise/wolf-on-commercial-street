6f2aa800f9fd4dc6dfea1f4797272f39d2a975f8
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-04 14:23:58 +0100

    Solution

--- lib/wolf_on_commercial_street.rb
+++ lib/wolf_on_commercial_street.rb
@@ -1,9 +1,16 @@
 class WolfOnCommercialStreet
 
   def initialize(stock_prices)
+    @stock_prices = stock_prices
   end
 
   def maximise_profits
+    minimum = @stock_prices.min
+    index_minimum = @stock_prices.find_index(minimum)
+    new_stock_prices = @stock_prices[index_minimum..-1]
+    maximum = new_stock_prices.max
+    index_maximum = new_stock_prices.find_index(maximum)
+    return "Buy on day #{index_minimum + 1} and sell on day #{index_maximum + index_minimum + 1}"
   end
 
 end
